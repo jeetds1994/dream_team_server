a346422308cbebb57a2543e87bbaa47cc4a70f19
wkhearn <wkhearn@gmail.com>
2017-08-15 16:49:23 -0400

    scraped for first swatch of clubs; changed pagination to show 100 clubs per page

--- app/controllers/api/v1/clubs_controller.rb
+++ app/controllers/api/v1/clubs_controller.rb
@@ -4,41 +4,41 @@ require 'nokogiri'
 class Api::V1::ClubsController < ApplicationController
   def index
     # render json: Club.all.order(:name)
-    paginate json: Club.all.order(:name), per_page: 50
+    paginate json: Club.all.order(:name), per_page: 100
 
-    clubs = Club.all
-    clubs.map do |club|
-
-      if club.badge
-        if club.badge.split('/')[-3].to_i != 50
-          badge = club.badge.split('/')
-          badge[-3] = 50
-          badge = badge.join('/')
-          club.badge = badge
-          club.save
-        end
-      else
-        name = I18n.transliterate(club.name).split(' ')
-        name1 = name[-2]
-        name2 = name[-1]
-
-        if name1
-          response = HTTParty.get("https://www.fifaindex.com/teams/?name=#{name1}+#{name2}")
-          parse_page = Nokogiri::HTML(response)
-          img = parse_page.css('#no-more-tables > table > tbody > tr > td:nth-child(1) > a > img')
-          img_url = img[0].attributes["src"].value
-          club.badge = "https://www.fifaindex.com#{img_url}"
-          club.save
-        else
-          response = HTTParty.get("https://www.fifaindex.com/teams/?name=#{name2}")
-          parse_page = Nokogiri::HTML(response)
-          img = parse_page.css('#no-more-tables > table > tbody > tr > td:nth-child(1) > a > img')
-          img_url = img[0].attributes["src"].value
-          club.badge = "https://www.fifaindex.com#{img_url}"
-          club.save
-        end
-      end
-    end
+    # clubs = Club.all
+    # clubs.map do |club|
+    #
+    #   if club.badge
+    #     if club.badge.split('/')[-3].to_i != 50
+    #       badge = club.badge.split('/')
+    #       badge[-3] = 50
+    #       badge = badge.join('/')
+    #       club.badge = badge
+    #       club.save
+    #     end
+    #   else
+    #     name = I18n.transliterate(club.name).split(' ')
+    #     name1 = name[-2]
+    #     name2 = name[-1]
+    #
+    #     if name1
+    #       response = HTTParty.get("https://www.fifaindex.com/teams/?name=#{name1}+#{name2}")
+    #       parse_page = Nokogiri::HTML(response)
+    #       img = parse_page.css('#no-more-tables > table > tbody > tr > td:nth-child(1) > a > img')
+    #       img_url = img[0].attributes["src"].value
+    #       club.badge = "https://www.fifaindex.com#{img_url}"
+    #       club.save
+    #     else
+    #       response = HTTParty.get("https://www.fifaindex.com/teams/?name=#{name2}")
+    #       parse_page = Nokogiri::HTML(response)
+    #       img = parse_page.css('#no-more-tables > table > tbody > tr > td:nth-child(1) > a > img')
+    #       img_url = img[0].attributes["src"].value
+    #       club.badge = "https://www.fifaindex.com#{img_url}"
+    #       club.save
+    #     end
+    #   end
+    # end
   end
 
   def show
